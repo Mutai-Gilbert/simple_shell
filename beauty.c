ff7f02c2c86b039c2af984ec4faddabc306c6783
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-08-31 05:03:04 +0000

    Beautiful code with betty

---
+++ beauty.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *  * main - Print a string
+ *   * Description: use printf
+ *    * Return: 0
+ */
+
+
+
+int main(void)
+
+{
+
+		printf("A beautiful code with betty moments,\n");
+
+			return (0);
+
+}
